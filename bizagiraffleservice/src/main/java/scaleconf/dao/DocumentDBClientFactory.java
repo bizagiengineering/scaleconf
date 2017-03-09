cdf21f89359da2e37e99bbef930a21515a42e770
Camilo Andrés Hernández Marin <camiloahm@gmail.com>
2017-03-09 14:53:29 -0500

    Config

--- bizagiraffleservice/src/main/java/scaleconf/dao/DocumentDBClientFactory.java
+++ bizagiraffleservice/src/main/java/scaleconf/dao/DocumentDBClientFactory.java
@@ -9,12 +9,12 @@ import com.microsoft.azure.documentdb.DocumentClient;
  * Created by dev-camiloh on 3/7/17.
  */
 public class DocumentDBClientFactory {
-    private static final String HOST = "https://docdb-java-sample.documents.azure.com:443/";
-    private static final String MASTER_KEY = "[YOUR_KEY_HERE]";
+    private static final String HOST = "https://scaleconf.documents.azure.com:443/";
+    private static final String MASTER_KEY = "L3ZxsbQ0DzwrwXx1EFazVOIOfLcpmEMHuP712PVi2L1jtD33I7gxVVf9AnHCGXR6E6Bgb347GlhA0oJTMOQ4lQ==";
 
     private static DocumentClient documentClient = new DocumentClient(HOST, MASTER_KEY,
             ConnectionPolicy.GetDefault(), ConsistencyLevel.Session);
-
+    
     public static DocumentClient getDocumentClient() {
         return documentClient;
     }
