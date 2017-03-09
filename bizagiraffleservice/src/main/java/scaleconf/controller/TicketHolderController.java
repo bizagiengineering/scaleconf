7f1ef5e4e0a0f4e949890037dc98483b4638f640
Felipe Rojas <wfelipe3@gmail.com>
2017-03-09 17:36:30 -0500

    upload file service

--- bizagiraffleservice/src/main/java/scaleconf/controller/TicketHolderController.java
+++ bizagiraffleservice/src/main/java/scaleconf/controller/TicketHolderController.java
@@ -1,10 +1,21 @@
 package scaleconf.controller;
 
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import scaleconf.dao.Holder;
 import scaleconf.dao.HolderDDBDao;
 import scaleconf.dao.HolderDao;
+import com.microsoft.azure.storage.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  * Created by dev-camiloh on 3/6/17.
@@ -18,5 +29,37 @@ public class TicketHolderController {
         Holder holder=new Holder("Camilo","http://url","http://document");
         holder=holderDao.createHolder(holder);
         return holder;
+    }
+
+    @PostMapping("/api/cv")
+    public String uploadCV(@RequestParam("cv") MultipartFile file) {
+        return uploadFile(createContainer("DefaultEndpointsProtocol=http;AccountName=scaleconf;AccountKey=Zl9MhlM/iiAiyIKdvqJvX3x3tWIEM0P5VfxYqRuQJbgxM7JMFlYnvOqq0YoWHLMSdwC36R/ogbqwcsk36ajDEw=="), file);
+    }
+
+    private CloudBlobContainer createContainer(String connection) {
+        try {
+            CloudStorageAccount storageAccount = CloudStorageAccount.parse(connection);
+            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+            CloudBlobContainer container = blobClient.getContainerReference("scaleconf");
+            container.createIfNotExists();
+            return container;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String uploadFile(CloudBlobContainer container, MultipartFile file) {
+        try {
+            System.out.println(file.getOriginalFilename());
+            CloudBlockBlob blob = container.getBlockBlobReference(file.getOriginalFilename());
+            blob.upload(new ByteArrayInputStream(file.getBytes()), file.getSize());
+            return blob.getUri().toString();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (StorageException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
