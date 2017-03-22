5fe7618805c3f7b655d4ca902e3be0fb43c8fbe0
Bizagi Dev <engineering@bizagi.com>
2017-03-22 12:20:22 -0500

    Update deployk8s.sh
    To Change expose

--- bizagiraffleservice/deployk8s.sh
+++ bizagiraffleservice/deployk8s.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "Delete previous deployment and service"
-kubectl delete deployment,service scaleconf --ignore-not-found --now
+kubectl delete deployment scaleconf --ignore-not-found --now
 
 echo "Create Kubernetes deployment"
 kubectl create -f scaleconf.yaml
 
 echo "Expose Kubernetes deployment"
-kubectl expose deployments scaleconf --port=8080 --target-port=8080 --type=LoadBalancer
